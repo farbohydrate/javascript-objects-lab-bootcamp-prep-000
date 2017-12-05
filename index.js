23f91832542f753912b72cc9504e42d85bc69108
Michael Farber <farbohydrate@gmail.com>
2017-12-05 01:30:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var recipes = {
 };
 
 function updateObjectWithKeyAndValue(object, key, value){
-  object[key] = value;
-  return object;
+  var newObj = Object.assign({}, object, { key: value });
+  return newObj;
 }
